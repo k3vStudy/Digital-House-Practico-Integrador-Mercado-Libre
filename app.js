da5fc563ecdae78f5be09024b6027c5722c84b6d
k3v <25158905+k3v53@users.noreply.github.com>
2023-12-05 11:26:58 -0300

    Finished forms

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.static('views'));
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'views', 'home.html'));
